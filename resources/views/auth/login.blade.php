b98f1b7cd0d3c8248c47035795af534d9fcc0f11
faridzag <faridzarhamgustin@gmail.com>
2024-01-19 11:16:16 +0700

    fix auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,23 +24,10 @@
                 <x-label for="password" value="{{ __('Password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
-
-            <!--
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Ingat Saya') }}</span>
-                </label>
-            </div>-->
-
             <div class="flex items-center justify-end mt-4">
-<!--
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Lupa Password?') }}
-                    </a>
-                @endif
--->
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Belum Memiliki Akun?') }}
+                </a>
                 <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
